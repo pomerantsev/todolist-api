336e8ed85a6db317e6a625706eb69c893a8e6685
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-12-07 14:27:51 +0400

    Add todo model validations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,7 @@
 class Todo < ActiveRecord::Base
   validates :title, presence: true,
                     length: { maximum: 140 }
+  validates :completed, inclusion: { in: [true, false] }
+  validates :priority, presence: true,
+                       inclusion: { in: 1..3 }
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -4,5 +4,16 @@ describe Todo do
   describe ".validations" do
     it { should validate_presence_of :title }
     it { should ensure_length_of(:title).is_at_most(140) }
+
+    it "doesn't allow nil in 'completed'" do
+      # ensure_inclusion_of doesn't work for boolean values:
+      # https://github.com/thoughtbot/shoulda-matchers/issues/179
+      expect(build(:todo, completed: true)).to be_valid
+      expect(build(:todo, completed: false)).to be_valid
+      expect(build(:todo, completed: nil)).to_not be_valid
+    end
+
+    it { should validate_presence_of :priority }
+    it { should ensure_inclusion_of(:priority).in_range(1..3) }
   end
 end
