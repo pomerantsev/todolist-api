06650893b37a308231ad38e0648b620d3f0dc685
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-12-07 16:44:47 +0400

    Add specs for the todo create action

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -12,8 +12,8 @@ describe TodosController do
   end
 
   describe "GET #show" do
+    let(:todo) { create :todo }
     context "when the todo exists" do
-      let!(:todo) { create :todo }
       it "responds with the requested todo" do
         get :show, id: todo
         expect(response.status).to eq(200)
@@ -25,6 +25,34 @@ describe TodosController do
       it "responds with a not_found response" do
         get :show, id: 1000
         expect(response.status).to eq(404)
+      end
+    end
+  end
+
+  describe "POST #create" do
+    before { post :create, todo: todo_params }
+    context "with a valid todo" do
+      let(:todo_params) { attributes_for(:todo) }
+      let(:todo) { Todo.last }
+      it "responds with 'created' status" do
+        expect(response.status).to eq(201)
+      end
+      it "renders the todo" do
+        expect(response.body).to eq todo.to_json
+      end
+      it "includes the path to the newly created record" do
+        expect(response.location).to eq todo_url(todo)
+      end
+    end
+
+    context "with an invalid todo" do
+      let(:todo_params) { attributes_for(:todo, title: "") }
+      let(:errors) { Todo.create(todo_params).errors }
+      it "responds with 'unprocessable entity' status" do
+        expect(response.status).to eq 422
+      end
+      it "renders the errors for the todo" do
+        expect(response.body).to eq errors.to_json
       end
     end
   end
