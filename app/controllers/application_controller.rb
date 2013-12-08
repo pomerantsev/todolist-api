5d46b09f669d118d69fd26797db64165baa7cc69
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-12-08 15:45:06 +0400

    Refactor the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,23 @@
 class ApplicationController < ActionController::API
   # CORS actions: http://stackoverflow.com/questions/17858178/allow-anything-through-cors-policy
-  before_action :cors_preflight_check
   after_action :cors_set_access_control_headers
 
+  def cors_preflight_check
+    set_headers
+    head :no_content
+  end
+
+private
+  
   def cors_set_access_control_headers
+    set_headers
+  end
+
+  def set_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, DELETE, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 
-  def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, DELETE, OPTIONS'
-      headers['Access-Control-Request-Method'] = '*'
-      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-      head :no_content
-    end
-  end
 end
