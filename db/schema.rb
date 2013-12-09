dc7575f8b9fafb2f6dd9cbde9b848be88cfe8a8c
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-12-09 21:59:54 +0400

    Migrate the DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131209072607) do
+ActiveRecord::Schema.define(version: 20131209175825) do
 
   create_table "todos", force: true do |t|
     t.string   "title"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20131209072607) do
     t.integer  "priority"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "todos", ["user_id"], name: "index_todos_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                default: "", null: false
