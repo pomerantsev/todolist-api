be72ffa9c0acf0a09b914558fc2d53d2375d57f2
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-12-09 11:45:25 +0400

    Restore deprecated token_authenticatable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,22 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :token_authenticatable
+  devise :database_authenticatable, :registerable
 
   before_save :ensure_authentication_token
+
+  def ensure_authentication_token
+    if authentication_token.blank?
+      self.authentication_token = generate_authentication_token
+    end
+  end
+
+private
+
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
+  end
 end
